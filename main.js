16937af61c2092251214d05f089f4af0c5593517
SG-sakthi <sakthiganesh782@gmail.com>
2023-07-12 14:54:14 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById('lbsinput').addEventListener('input',
+function(e){
+      let lbs=e.target.value;
+      document.getElementById('gramsOutput').innerHTML=lbs/0.0022046;
+     
+      document.getElementById('kgOutput').innerHTML=lbs/2.2046;
+      document.getElementById('ozOutput').innerHTML=lbs*16;
+})
